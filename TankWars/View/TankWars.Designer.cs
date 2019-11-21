a4c5a25c753f34a87b591184a45b63b69d38722a
Jonathan Ryan Wigderson <jrwigd@gmail.com>
2019-11-20 21:29:05 -0700

    Tank now fires

--- TankWars/View/TankWars.Designer.cs
+++ TankWars/View/TankWars.Designer.cs
@@ -39,17 +39,19 @@
             // 
             this.ServerText.AutoSize = true;
             this.ServerText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ServerText.Location = new System.Drawing.Point(13, 13);
+            this.ServerText.Location = new System.Drawing.Point(17, 16);
+            this.ServerText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.ServerText.Name = "ServerText";
-            this.ServerText.Size = new System.Drawing.Size(54, 17);
+            this.ServerText.Size = new System.Drawing.Size(63, 20);
             this.ServerText.TabIndex = 0;
             this.ServerText.Text = "Server:";
             // 
             // ServerInput
             // 
-            this.ServerInput.Location = new System.Drawing.Point(82, 15);
+            this.ServerInput.Location = new System.Drawing.Point(109, 18);
+            this.ServerInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ServerInput.Name = "ServerInput";
-            this.ServerInput.Size = new System.Drawing.Size(152, 20);
+            this.ServerInput.Size = new System.Drawing.Size(201, 22);
             this.ServerInput.TabIndex = 1;
             this.ServerInput.Text = "localhost";
             // 
@@ -57,26 +59,29 @@
             // 
             this.NameText.AutoSize = true;
             this.NameText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.NameText.Location = new System.Drawing.Point(249, 13);
+            this.NameText.Location = new System.Drawing.Point(332, 16);
+            this.NameText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.NameText.Name = "NameText";
-            this.NameText.Size = new System.Drawing.Size(49, 17);
+            this.NameText.Size = new System.Drawing.Size(58, 20);
             this.NameText.TabIndex = 2;
             this.NameText.Text = "Name:";
             // 
             // NameInput
             // 
-            this.NameInput.Location = new System.Drawing.Point(313, 15);
+            this.NameInput.Location = new System.Drawing.Point(417, 18);
+            this.NameInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.NameInput.Name = "NameInput";
-            this.NameInput.Size = new System.Drawing.Size(152, 20);
+            this.NameInput.Size = new System.Drawing.Size(201, 22);
             this.NameInput.TabIndex = 3;
             this.NameInput.Text = "player";
             // 
             // ConnectButton
             // 
             this.ConnectButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ConnectButton.Location = new System.Drawing.Point(504, 10);
+            this.ConnectButton.Location = new System.Drawing.Point(672, 12);
+            this.ConnectButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ConnectButton.Name = "ConnectButton";
-            this.ConnectButton.Size = new System.Drawing.Size(132, 26);
+            this.ConnectButton.Size = new System.Drawing.Size(176, 32);
             this.ConnectButton.TabIndex = 4;
             this.ConnectButton.Text = "Connect";
             this.ConnectButton.UseVisualStyleBackColor = true;
@@ -84,20 +89,23 @@
             // 
             // TankWars
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(884, 861);
+            this.ClientSize = new System.Drawing.Size(1179, 1055);
             this.Controls.Add(this.ConnectButton);
             this.Controls.Add(this.NameInput);
             this.Controls.Add(this.NameText);
             this.Controls.Add(this.ServerInput);
             this.Controls.Add(this.ServerText);
             this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "TankWars";
             this.Text = "TankWars";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TankWars_KeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.TankWars_KeyUp);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TankWars_MouseDown);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.TankWars_MouseUp);
             this.ResumeLayout(false);
             this.PerformLayout();
 
