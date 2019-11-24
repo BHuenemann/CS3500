6478fcc57770a764dd0168a88767dc0123a70ff2
Ben Huenemann <ben41huenemann@gmail.com>
2019-11-24 16:46:55 -0700

    Added server basics

---
+++ TankWars/Server/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetworkUtil;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Networking.StartServer(SendStartupInfo, 11000);
+        }
+
+
+        private static void SendStartupInfo(SocketState obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
