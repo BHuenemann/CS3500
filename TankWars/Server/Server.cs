f1889caeb9e15001f023329319a493c583bf0941
Jonathan Ryan Wigderson <jrwigd@gmail.com>
2019-12-01 20:38:40 -0700

    messed with getting the projectiles and beams to spawn

--- TankWars/Server/Server.cs
+++ TankWars/Server/Server.cs
@@ -24,6 +24,8 @@ namespace Server
         static private int FramesPerShot;
         static private int RespawnRate;
 
+        Stopwatch FireRateStopWatch = new Stopwatch();
+
 
         public static void Main(string[] args)
         {
@@ -96,11 +98,18 @@ namespace Server
                 TheWorld.TankSetAiming(t, TheWorld.PlayerCommands[t.ID].aiming);
 
                 //FIRING
-                switch (PlayerCommands[t.ID].fire)
+                switch (TheWorld.PlayerCommands[t.ID].fire)
                 {
                     case "main":
+                        if(t.OnCooldown == false)
+                        {
+                            Projectile p = new Projectile(t.location, t.orientation, t.ID);
+                            TheWorld.Projectiles.Add(p.ID, p);
+                        }
                         break;
                     case "alt":
+                        Beam b = new Beam(t.location, t.orientation, t.ID);
+                        TheWorld.Beams.Add(b.ID, b);
                         break;
                     case "none":
                         break;
