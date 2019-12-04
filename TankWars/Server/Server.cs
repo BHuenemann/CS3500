fab6ed98f00e686da19aa590146450b005894379
Jonathan Ryan Wigderson <jrwigd@gmail.com>
2019-12-03 20:03:10 -0700

    literally  nothing

--- TankWars/Server/Server.cs
+++ TankWars/Server/Server.cs
@@ -652,12 +652,12 @@ namespace Server
 
         public static void WrapAround(Tank t)
         {
-            if(Math.Abs(t.Location.GetX()) + Constants.TankSize/2  > TheWorld.worldSize/2)
+            if (Math.Abs(t.Location.GetX()) + Constants.TankSize / 2 > TheWorld.worldSize)
             {
                 TheWorld.TankSetLocation(t.ID, new Vector2D(Math.Sign(t.Location.GetX()) * -TheWorld.worldSize / 2, t.Location.GetY()));
             }
 
-            else if(Math.Abs(t.Location.GetY()) + Constants.TankSize/2 > TheWorld.worldSize/2)
+            else if (Math.Abs(t.Location.GetY()) + Constants.TankSize / 2 > TheWorld.worldSize / 2)
             {
                 TheWorld.TankSetLocation(t.ID, new Vector2D(t.Location.GetX(), Math.Sign(t.Location.GetY()) * -TheWorld.worldSize / 2));
             }
